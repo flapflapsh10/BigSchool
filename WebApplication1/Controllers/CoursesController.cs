8f191713fce8dbfba6889c803035972473916aef
Administrator <Administrator@PC226>
2020-06-09 10:22:37 +0700

    Add project files.

---
+++ WebApplication1/Controllers/CoursesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                Datetime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+}
---
+++ WebApplication1/Migrations/202006090208192_CreateTableCourse.cs
@@ -0,0 +1,48 @@
+namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTableCourse : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        LecturerId = c.String(nullable: false),
+                        Place = c.String(nullable: false, maxLength: 255),
+                        Datetime = c.DateTime(nullable: false),
+                        CategoryId = c.Byte(nullable: false),
+                        Leturer_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.Leturer_Id)
+                .Index(t => t.CategoryId)
+                .Index(t => t.Leturer_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Courses", "Leturer_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Courses", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Courses", new[] { "Leturer_Id" });
+            DropIndex("dbo.Courses", new[] { "CategoryId" });
+            DropTable("dbo.Courses");
+            DropTable("dbo.Categories");
+        }
+    }
+}
